c8bc77677777cf10bededce33d0d65ac630776f0
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-04-24 23:07:44 +0100

    Added login/logout logic to nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,62 @@
 import React from 'react';
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import UserContext from '../contexts/user.js';
 
-function Nav() {
-	return (
-		<Menu theme="dark" defaultSelectedKeys={['1']} mode="horizontal">
-			<Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-		</Menu>
-	);
+class Nav extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleLogout = this.handleLogout.bind(this);
+		this.handleNav = this.handleNav.bind(this);
+	}
+
+	handleLogout() {
+		const { user, logout } = this.context;
+		fetch('http://localhost:3000/api/v1/auth/logout', {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${user.accessToken}`
+			}
+		})
+			.catch((err) => console.error(err));
+		logout();
+	}
+
+	handleNav() {
+		const { loggedIn } = this.context;
+		let loginNav;
+		if (!loggedIn) {
+			loginNav = (
+				<>
+					<Menu.Item key="3" style={{ float: 'right' }}>
+						<Link to="/register">Register</Link>
+					</Menu.Item>
+					<Menu.Item key="4" style={{ float: 'right' }}>
+						<Link to="/login">Login</Link>
+					</Menu.Item>
+				</>
+			);
+		} else {
+			loginNav = (
+				<Menu.Item key="3" onClick={this.handleLogout} style={{ float: 'right' }}>
+					<Link to="/">Logout</Link>
+				</Menu.Item>
+			);
+		}
+		return loginNav;
+	}
+
+	render() {
+		return (
+			<Menu theme="dark" defaultSelectedKeys={['1']} mode="horizontal">
+				<Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+				<Menu.Item key="2"><Link to="/favourite">Favourites</Link></Menu.Item>
+				{ this.handleNav()}
+			</Menu>
+		);
+	}
 }
+
+Nav.contextType = UserContext;
 
 export default Nav;
