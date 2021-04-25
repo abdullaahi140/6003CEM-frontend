32c38181d08048808b3aacbc8f6d716ab69e152f
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-04-25 02:51:47 +0100

    Refactored to use grid fetch all dogs Uses the recently changed grid component to fetch dogs and send the array as props.

--- src/components/home.js
+++ src/components/home.js
@@ -5,10 +5,14 @@ import DogGrid from './doggrid.js';
 import Page from './pagination.js';
 import SearchBar from './search.js';
 
+import { json, status } from '../utilities/requestHandlers.js';
+
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			dogs: [],
+			loading: true,
 			page: 1,
 			name: '',
 			breed: ''
@@ -16,6 +20,16 @@ class Home extends React.Component {
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
 		this.handleSelect = this.handleSelect.bind(this);
+		this.fetchDogs = this.fetchDogs.bind(this);
+	}
+
+	componentDidMount() {
+		this.fetchDogs();
+	}
+
+	componentDidUpdate() {
+		const { page, name, breed } = this.state;
+		this.fetchDogs(page, name, breed);
 	}
 
 	handleChange(page) {
@@ -31,8 +45,23 @@ class Home extends React.Component {
 		this.setState({ breed: value });
 	}
 
+	fetchDogs(page = 1, name = '', breed = '') {
+		fetch(`http://localhost:3000/api/v1/dogs?page=${page}&name=${name}&breed=${breed}`)
+			.then(status)
+			.then(json)
+			.then((data) => {
+				this.setState({ dogs: data.dogs, loading: false });
+			})
+			.catch((err) => {
+				this.setState({ dogs: [] });
+				console.error(err, 'Error fetching dogs');
+			});
+	}
+
 	render() {
-		const { name, breed, page } = this.state;
+		const {
+			dogs, loading, name, breed, page
+		} = this.state;
 		return (
 			<div className="site-layout-content">
 				<div style={{ padding: '0% 10% 1%', textAlign: 'center' }}>
@@ -44,7 +73,7 @@ class Home extends React.Component {
 					/>
 					<SearchBar onSearch={this.handleSearch} onSelect={this.handleSelect} />
 				</div>
-				<DogGrid {...this.state} />
+				<DogGrid dogs={dogs} loading={loading} />
 				<div style={{ display: 'flex', justifyContent: 'center' }}>
 					<Page
 						resource="dogs"
