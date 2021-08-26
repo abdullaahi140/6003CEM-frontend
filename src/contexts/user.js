54af9d3469d7858f7dcbe36ae7f9dcbe4c01e55a
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-08-26 14:54:36 +0100

    Remove JS files Will be convered to TS files.

--- src/contexts/user.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-/** User context for storing the users detail. To be used by components in the SPA. */
-const UserContext = React.createContext();
-UserContext.displayName = 'User Context';
-
-export default UserContext;
